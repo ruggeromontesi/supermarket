b48061932fee53fa3ac426ab6f47d2bc624e0594
montesr <ruggero.montesi@dematic.com>
2022-05-11 05:15:27 +0300

    ready to be submitted

--- install.bat
+++ install.bat
@@ -2,5 +2,4 @@ call git clone  https://github.com/ruggeromontesi/supermarket.git
 cd  supermarket
 call mvn clean install -f pom.xml
 call mvn compile exec:java -Dexec.mainClass="org.reiz.App"  
-cd target/generated-reports
 cmd /k
